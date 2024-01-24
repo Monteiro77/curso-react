9835b55a27500704cba6cd20497c8bb67ffb9707
Monteiro77 <monteiro240905@gmail.com>
2024-01-24 10:31:25 -0300

    hello word

---
+++ AULA 1 - TESTE/Hello Word 2/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Hello Word 2</h1>
+    <div className='container'>
+      <span>Vinícius Monteiro Araújo</span>
+    </div>
+    </>
+  )
+}
+
+export default App
